ef34f13632346861e53864cd36cad07553f3038d
jordankelley123 <142934283+jordankelley123@users.noreply.github.com>
2024-03-21 13:53:23 -0700

    Add files via upload

---
+++ CISC191FinalProject/src/java/pack/TestFinalProject2.java
@@ -0,0 +1,16 @@
+package java.pack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestFinalProject2
+{
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
