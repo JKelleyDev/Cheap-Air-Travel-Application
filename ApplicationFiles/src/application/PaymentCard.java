4cef58ecd71b7682fd1da35eb20945b1b8f06636
jordankelley123 <jordan.d.kelley1@gmail.com>
2024-04-02 15:39:49 -0700

    Restored deleted files

---
+++ ApplicationFiles/src/application/PaymentCard.java
@@ -0,0 +1,56 @@
+package application;
+
+
+public class PaymentCard  
+{	
+	private String cardNumber; 
+	private String cardHolderName; 
+	private String expiryDate; 
+	private String cvv;
+	
+	
+	//////////////////////////////////// Setter Methods 
+	
+	public void setCardNumber(String cardNumber)
+	{ 
+		this.cardNumber = cardNumber; 
+	}
+	
+	public void setCardHolderName(String cardHolderName)
+	{ 
+		this.cardHolderName = cardHolderName; 
+	}
+	
+	public void setExpiryDate(String expiryDate)
+	{ 
+		this.expiryDate = expiryDate; 
+	}
+	
+	public void setCVV(String cvv)
+	{ 
+		this.cvv = cvv; 
+	}
+	
+	////////////////////////////////// Getter Methods 
+	
+	public String getCardNumber() 
+	{ 
+		return cardNumber; 
+	}
+	
+	public String getcardHolderName()
+	{ 
+		return cardHolderName; 
+	}
+	
+	public String getExpiryDate() 
+	{ 
+		return expiryDate; 
+	}
+	
+	public String getCVV() 
+	{ 
+		return cvv; 
+	}
+	
+}
---
+++ ApplicationFiles/src/application/TestFinalProject2.java
@@ -0,0 +1,17 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestFinalProject2
+{
+
+	@Test
+	void test()
+	{ 
+		System.out.print("Testing Branch");
+	}
+
+}
+
