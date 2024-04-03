48eb3e1f37e93a487f3d48c25365b6a92254cdd4
jordankelley123 <jordan.d.kelley1@gmail.com>
2024-04-02 17:37:28 -0700

    Fixed Project Name and Path

---
+++ TravelWIthUs/src/application/MealOptions.java
@@ -0,0 +1,37 @@
+package application;
+
+public class MealOptions
+{
+	private Traveler traveler; 
+	
+	public MealOptions(Traveler traveler)
+	{ 
+		this.traveler = traveler; 
+	}
+	
+	/// Create Meal Options 
+	
+		// Example 
+		/* 
+		 * String Meal_1_Name = "Steak and Potatoes"; 
+		 * 
+		 * int Meal_1_Calories = 560; 
+		 * 
+		 * double Meal_1_Price = 7.65; 
+		 * 
+		 * 
+		 */
+	
+	
+	// Create Getter Methods 
+	
+	// Return the name of the meal 
+	
+	// Return the price of the meal 
+	
+	// Return the calories of the meal 
+	
+	
+	// You do NOT need setter methods 
+	
+}
